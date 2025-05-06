6beb3df61b1122fdd845fdda4d78eaeb9f8c1b0a
Saul André <saulandre@gmail.com>
2025-05-06 16:14:01 -0300

    salvando a cor do botão

--- src/components/Authenticated/Header.jsx
+++ src/components/Authenticated/Header.jsx
@@ -295,7 +295,7 @@ const HeaderMain = ({className }) => {
       <Container>
         <Header className={className}>
           <Wrapper><ImagemResponsiva src="/favicon.png" alt="Banner do evento" />
-          <Title>INSCRIÇÕES XXXV CONMEL 2025</Title>
+          <Title>XXXV CONMEL 2025</Title>
     </Wrapper>          <Nav>
 
 
